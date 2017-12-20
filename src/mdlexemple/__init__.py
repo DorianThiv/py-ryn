619649b5a96f18bb5f5d032a8b7bc2b9b43111d1
Tivl <dorian_thivolle@orange.fr>
2017-12-20 19:40:30 +0100

    Add docstring in __init__.py mdl exemple

--- src/mdlexemple/__init__.py
+++ src/mdlexemple/__init__.py
@@ -1,4 +1,45 @@
 
+# -*- coding: utf-8 -*-
+"""Example Python style docstrings.
+
+This module demonstrates documentation as specified by the `Google Python
+Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+with a section header and a colon followed by a block of indented text.
+
+Example:
+    Examples can be given using either the ``Example`` or ``Examples``
+    sections. Sections support any reStructuredText formatting, including
+    literal blocks::
+
+        $ python core.py
+
+Section breaks are created by resuming unindented text. Section breaks
+are also implicitly created anytime a new section starts.
+
+Attributes:
+    module_level_variable1 (int): Module level variables may be documented in
+        either the ``Attributes`` section of the module docstring, or in an
+        inline docstring immediately following the variable.
+
+        Either form is acceptable, but the two should not be mixed. Choose
+        one convention to document module level variables and be consistent
+        with it.
+
+Todo:
+    * For module TODOs
+    * You have to also use ``sphinx.ext.todo`` extension
+
+.. Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+
+"""
+
+# package name important to found right classes
+package = "mdlexemple"
+
+"""int: Module level variable documented inline.
+"""
+
 import os
 import sys
 import inspect
@@ -7,9 +48,16 @@ from mdlexemple.providers import *
 from mdlexemple.registries import *
 from mdlexemple.binders import *
 
-package = "mdlexemple"
 
 def __getclasses():
+
+    """
+    Get classes function return all classes in this module.
+    Args:
+        * No params
+    Returns:
+        list: array with few classes founded in modules : [providers, registries, binders]
+    """
     ret = []
     clss = inspect.getmembers(sys.modules[__name__], inspect.isclass)
     for c in clss:
