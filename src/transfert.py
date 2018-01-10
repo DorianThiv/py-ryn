f9525eb5ed4c8d7de1032864b34a072ac01b11dd
Dorian Thivolle <dorian.thivolle@etu.univ-grenoble-alpes.fr>
2018-01-10 22:34:26 +0000

    Add docstring in FrameTransfert classes

--- src/transfert.py
+++ src/transfert.py
@@ -8,15 +8,10 @@ class FrameDecoder:
 
 class SimpleFrameTransfert:
 
-    """ This frame define the internal tranfert protocole
-        
-        Data Types : 
-        * Emitter : (id) ""
-        * Receptor : (id) ""
-        * Direction : (read | write) ""
-        * Payload : (json) ""
-        * Timestamp : (tmestamp) ""
-        * Crc : (string) "" 
+    """ Serialize @param : frame to SimpleFrameTransfert :
+        Test:   [Command] => BaseCommand (actually BaseAction)
+                [Callback] => Optionnal
+                [Log] => class Logger (message, exception, comments) ???   
     """
 
     def __init__(self, sort=0, direction=None, payload=None):
@@ -27,7 +22,19 @@ class SimpleFrameTransfert:
     def __str__(self):
         return "__FRAME__ = (|em : {} | dir: {} | payload : {})".format(self.sort, self.direction, self.payload)
 
+    @staticmethod
+    def serialize(frame):
+        pass
+
 class ModuleFrameTransfert:
+
+    """ Serialize @param : frame to SimpleFrameTransfert :
+        Test:   [Address Dest] => (M = 0x00, P = 0x00, R = 0x00, B = 0x00)
+                [Command] => BaseCommand (actually BaseAction)
+                [Payload] => Content known buy dest module
+                [Callback] => Optionnal
+                [Log] => class Logger (message, exception, comments) ???   
+    """
 
     """ This frame define the internal tranfert protocole
         
