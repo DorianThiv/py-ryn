6748d6707b7e0dea720436030d1e36aa3a002681
Tivl <dorian_thivolle@orange.fr>
2018-01-14 17:53:06 +0100

    Modify command terminal-manager __doc__

--- src/mdlterminal/managers.py
+++ src/mdlterminal/managers.py
@@ -15,12 +15,21 @@ class TerminalManager(BaseManager):
 	def command(self, command):
 		""" Terminal Module command :
 			* mdlterminal [(-r or --read) or (-w | --write)] [(-a or --address) 0.0.0.0)] [(-t or --text) 'your text']
+
+			Args:
+				* command: string
+			Returns:
+				* tuple(True, dict)
+				* tuple(False, error: string)
 		"""
+		commanddict = {}
 		splitted = command.split(" ")
 		for i in range(splitted):
 			if re.match(r"mdl([a-z])+", splitted[i]) == None and splitted[i] == self.module:
 				error = "module name doesn't match"
 				return (False, error)
+			else:
+				
 			if re.match(r"(-|-{2})+", splitted[i]) == None:
 				error = "no arguments in this command"
 				return (False, error)
