5d92aaad99776a27687e3aa4355b257e6f041aac
DorianT <dorianth@orange.fr>
2018-02-08 13:50:31 +0100

    Testing queue

--- test/test_ryn_queue_subscribe.py
+++ test/test_ryn_queue_subscribe.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python 3
 
+import queue
+
 class Registry:
 
     def __init__(self):
         self.directory = {}
     
-    def subscribe(self, name, ):
-        pass
+    def subscribe(self, name, command):
+        if name in list(self.directory.keys()):
+            self.directory[name].put(command)
+        else:
+            self.directory[name] = queue.Queue()
+            self.directory[name].put(command)
 
     def unsubscribe(self):
         pass
 
     def __str__(self):
-        return self.directory
+        ret = ""
+        for c in self.directory:
+            ret += c + "\n"
+        return ret
+    
+if __name__ == "__main__":
+    
+    reg = Registry()
+    reg.subscribe("mdldb", "write")
+    reg.subscribe("mdldb", "read")
+    reg.subscribe("mdlmodbus", "read")
+    print(reg)
