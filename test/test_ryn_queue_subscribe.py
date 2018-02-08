c461610cdf13481685be31a785c0d2a517f63d76
DodoT <dorian_thivolle@orange.fr>
2018-02-08 10:52:07 +0100

    Add test logger

---
+++ test/test_ryn_queue_subscribe.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python 3
+
+class Registry:
+
+    def __init__(self):
+        self.directory = {}
+    
+    def subscribe(self, name, ):
+        pass
+
+    def unsubscribe(self):
+        pass
+
+    def __str__(self):
+        return self.directory
